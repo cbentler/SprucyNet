51dfe5cf00b5994dd5952f96831e0f42c9fa2574
cbentler <crbentler@gmail.com>
2016-08-24 00:51:09 -0400

    User Creation
    Upated user creation to redirect to actCreation and made actCreation.php
    to add new users to the database (and update maxnum vals)

---
+++ actCreation.php
@@ -0,0 +1,48 @@
+<!--SprucyPay Account Creation Script 8-24-16 v.0.0.2-->
+
+<?php
+$servername = "localhost";
+$username = "cbentle";
+$password = "guest";
+$dbname = "sprucypay";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+   }
+
+   $sql = "INSERT INTO user (usernum, username, password, email, amtowed, fname, lname)
+   SELECT maxnumval,
+   \"{$_POST['username']}\",
+   \"{$_POST['password']}\",
+   \"{$_POST['email']}\",
+   0,
+   \"{$_POST['fname']}\",
+   \"{$_POST['lname']}\"
+
+
+   from
+   sprucypay.maxnum
+   where
+   maxnum = 1";
+
+   if ($conn->query($sql) === TRUE) {
+       echo "New record created successfully";
+   } else {
+       echo "Error: " . $sql . "<br>" . $conn->error;
+   }
+
+   $update = "update sprucypay.maxnum set maxnumval = maxnumval + 1 where maxnum = 1";
+
+   if ($conn->query($update) === TRUE) {
+       echo "Update Successful";
+   } else {
+       echo "Error: " . $update . "<br>" . $conn->error;
+   }
+
+   $conn->close();
+
+
+?>
