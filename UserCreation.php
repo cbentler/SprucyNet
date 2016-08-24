993be58502c1f4ae194cf25b9ba25fff6869a037
cbentler <crbentler@gmail.com>
2016-08-23 23:19:17 -0400

    User Creation
    html to php file

---
+++ UserCreation.php
@@ -0,0 +1,63 @@
+<!--SprucyPay User Creation Page 8-19-16 v.0.0.1-->
+
+<HTML>
+  <head>
+    <title>
+      SprucyPay User Creation
+    </title>
+    <script>
+    </script>
+  </head>
+  <body>
+    <form method="POST">
+      <table>
+        <tr>
+          <td>
+            User Name:
+          </td>
+          <td>
+            <input type="text" id="username"/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Email:
+          </td>
+          <td>
+            <input type="text" id="email"/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Password:
+          </td>
+          <td>
+            <input type="text" id="password"/>
+          </td>
+        <tr>
+          <td>
+            First Name:
+          </td>
+          <td>
+            <input type="text" id="fname"/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Last Name:
+          </td>
+          <td>
+            <input type="text" id="lname"/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="submit" id="submit"/>
+          </td>
+          <td>
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</HTML>
