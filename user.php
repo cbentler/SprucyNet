f764c4ae500c63dc9e5fa9235f9dd2ab68b61750
cbentler <crbentler@gmail.com>
2016-08-26 18:18:29 -0400

    User.php rewrite
    Rewrite of the user.php file in response to overwriting the correct
    version previously.  :(

--- user.php
+++ user.php
@@ -15,16 +15,17 @@
         $sql = "SELECT * from user WHERE usernum > 0";
         $result = $conn->query($sql);
 
-        if ($result->num_rows > 0) {
+
               echo "results";
+              $var = '';
              // output data of each row
-             /*while($row = $result->fetch_assoc()) {
-                 //echo "<br> id: ". $row["requestnum"]. " - Name: ". $row["requestor"]. " " . $row["status"] . "<br>";
-                 echo '<tr><td>'. $row["requestnum"].'</td><td>'. $row["requestor"].'</td><td>'. $row["title"].'</td><td>'. $row["mediatype"].'</td><td>'. $row["requestdate"].'</td><td>'. $row["comments"].'</td><td><input type="button" id="deleteButton'. $row["requestnum"].'" name="deleteButton" value="?" onclick="q()"/></td></tr>';
-             }*/
-        } else {
-             echo "0 results";
-        }
+             while($row = $result->fetch_assoc()) {
+
+                 $a = '<tr><td>'. $row["usernum"].'</td><td>'. $row["username"].'</td><td>'. $row["email"].'</td><td>'. $row["fname"].'</td><td><input type="button" id="pwReset'. $row["usernum"].'" name="pwReset" value="?"/></td></tr>';
+                 $var .= $a;
+             }
+
+             echo ($var);
 
         $conn->close();
 ?>
