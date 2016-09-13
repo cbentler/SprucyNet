a1b23e5ae74f669ad9f76380cf299d144482d729
cbentler <crbentler@gmail.com>
2016-09-12 23:05:22 -0400

    User group update
    Major upate beginning the configuraiton of the user group tab on the
    admin page.
    user group list is now pulled
    user list is now populated from the users in each user group when the
    edit user group option is selected

--- database.php
+++ database.php
@@ -62,24 +62,74 @@ class query{
     $conn->close();
   }
 
+  function getUserGrpTable(){
+    $servername = "localhost";
+    $username = "cbentle";
+    $password = "guest";
+    $dbname = "sprucynet";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $sql = "SELECT * from usergrp where usergrpnum > 0 ORDER BY usergrpnum";
+    $result = $conn->query($sql);
+    $optHTML = '';
+    while($row = $result->fetch_assoc()){
+      $opt = '<tr><td>'.$row["usergrpnum"].'</td><td>'.$row["usergrpname"].'</td><td>'.$row["grouptype"].'</td><td><button type="button" class="editBtn" id="'. $row["usergrpnum"].'" name="editBtn'. $row["usergrpnum"].'" onClick="editUserGrp(this.id);"><img src="resources/pencil.png"></button></td></tr>';
+      $optHTML .= $opt;
+    }
+    echo ($optHTML);
+    $conn->close();
+  }
+
+  function userGrpEdit($param){
+    $servername = "localhost";
+    $username = "cbentle";
+    $password = "guest";
+    $dbname = "sprucynet";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+//out of user group
+    $sql1 = "SELECT username from user where usernum not in (select usernum from uxug where usergrpnum = $param)";
+    $result1 = $conn->query($sql1);
+    $optHTML = '<select multiple="multiple" class="ugassign" id="available">';
+    while($row = $result1->fetch_assoc()){
+      $opt = '<option value="'.$row["username"].'">'.$row["username"].'</option>';
+      $optHTML .= $opt;
+    }
+//add button
+      $optHTML .= '</select><button type="button" id="add" class="ugbtn" onClick="ugAddSelected();">Add</button><button id="remove" type="button" onClick="ugRemoveSelected();"class="ugbtn">Remove</button><select multiple="multiple" class="ugassign" id="assigned">';
+
+//in user group
+    $sql2 ="SELECT username from user where usernum in (select usernum from uxug where usergrpnum = $param)";
+    $result2 = $conn->query($sql2);
+    while($row = $result2->fetch_assoc()){
+      $opt = '<option value="'.$row["username"].'">'.$row["username"].'</option>';
+      $optHTML .= $opt;
+    }
+
+    echo ($optHTML);
+    $conn->close();
+  }
+
   function onLoad(){
         if (isset($_POST['action'])) {
           $action = $_POST['action'];
-
+          if(isset($_POST['param'])){
+            $param = $_POST['param'];
+            $this->userGrpEdit($param);
+          }else{
           $servername = "localhost";
           $username = "cbentle";
           $password = "guest";
           $dbname = "sprucynet";
-
           // Create connection
           $conn = new mysqli($servername, $username, $password, $dbname);
-
           if ($conn->connect_error) {
              die("Connection failed: " . $conn->connect_error);
            }
-
-
-
            switch($action){
              case 'user':
              $this->getUserDS();
@@ -93,14 +143,16 @@ class query{
              $this->getCats();
              break;
 
+             case 'usergrptable' :
+             $this->getUserGrpTable();
+             break;
            }
-
-        }
-        else{
-
+         }
+        }else{
         }
       }
-}
+    }
+
 
 $start = new query();
 $start->onLoad();
